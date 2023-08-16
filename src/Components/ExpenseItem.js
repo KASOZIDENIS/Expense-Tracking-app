091350835ad29ba7c3240c376f18aba83582a1b8
KASOZI DENIS <dkasozi247@gmail.com>
2023-08-16 12:50:46 +0300

    Expense management

---
+++ src/Components/ExpenseItem.js
@@ -0,0 +1,13 @@
+function ExpenseItem(){
+    return(
+        <div>
+            <div>16/08/2023</div>
+            <div>
+                <h1>Car Insurance</h1>
+                <div>$46.65</div>
+                </div>            
+        </div>
+    );
+}
+
+export default ExpenseItem;
